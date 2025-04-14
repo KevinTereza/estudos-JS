5868f722478ce29fddb460d6a4228541e16d4924
Kevin Tereza <kevinds.tereza@gmail.com>
2025-04-14 20:53:32 -0300

    Terminando a função de escalar jogador

--- exercicios/exercicio-11/script.js
+++ exercicios/exercicio-11/script.js
@@ -2,13 +2,34 @@ function lineUpPlayer() {
     const playerName = document.getElementById('player-name').value;
     const playerPosition = document.getElementById('player-position').value;
     const playerNumber = document.getElementById('player-number').value;
-
-    let userChoice = confirm(`Você quer escalar o jogador:\nNome ${playerName}\nPosição ${playerPosition}\nNúmero: ${playerNumber}`);
-    if (userChoice) {
-        const playersDiv = document.getElementById('team-playes');
-        const playersList = document.createElement('ul')
+    
+    if (playerName === '' || playerPosition === '' || playerNumber === '') {
+        alert('Por favor, insira informações nos campos para continuar.');
+        return; 
     }
 
-    
+    let userChoice = confirm(`Você quer escalar o jogador:\nNome: ${playerName}\nPosição: ${playerPosition}\nNúmero: ${playerNumber}`);
+    if (userChoice) {
+        const playersDiv = document.getElementById('team-players');
+        const playersList = document.createElement('ul');
 
+        const playerInfo = document.createElement('ul');
+        const playerInfoLegend = document.createElement('h2')
+        const playerInfoName = document.createElement('li');
+        const playerInfoPosition = document.createElement('li');
+        const playerInfoNumber = document.createElement('li');
+
+        playerInfoLegend.innerText = `Informações do jogador:`;
+        playerInfoName.innerText = `Nome: ${playerName}`;
+        playerInfoPosition.innerText = `Posição: ${playerPosition}`;
+        playerInfoNumber.innerText = `Número: ${playerNumber}`;
+
+
+        playerInfo.append(playerInfoLegend, playerInfoName, playerInfoPosition, playerInfoNumber);
+        playersList.appendChild(playerInfo);
+        playersDiv.appendChild(playersList);
+
+        document.getElementById('lineup-form').reset();
+
+    }
 }
